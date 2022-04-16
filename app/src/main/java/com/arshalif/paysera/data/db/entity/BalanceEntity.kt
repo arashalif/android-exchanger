2a135b6fb159d6c6f62d1357867e84234767a5ba
arash <aghelifar.arash@gmail.com>
2022-04-16 18:34:34 +0430

    impl mapper entity to domain model

--- app/src/main/java/com/arshalif/paysera/data/db/entity/BalanceEntity.kt
+++ app/src/main/java/com/arshalif/paysera/data/db/entity/BalanceEntity.kt
@@ -3,6 +3,7 @@ package com.arshalif.paysera.data.db.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.arshalif.paysera.domain.model.Balance
 import java.math.BigDecimal
 
 @Entity
@@ -10,3 +11,10 @@ data class BalanceEntity(
     @PrimaryKey val type: String,
     val value: BigDecimal,
 )
+
+fun BalanceEntity.toBalance() =
+    Balance(type, value)
+
+fun List<BalanceEntity>.toVenueList() = map {
+    it.toBalance()
+}
