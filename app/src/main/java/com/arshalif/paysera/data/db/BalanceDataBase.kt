9d23efd52836538de09a4b5b0836a03c132a1210
arash <aghelifar.arash@gmail.com>
2022-04-16 18:12:59 +0430

    add balanceEntity to BalanceDataBase

--- app/src/main/java/com/arshalif/paysera/data/db/BalanceDataBase.kt
+++ app/src/main/java/com/arshalif/paysera/data/db/BalanceDataBase.kt
@@ -1,10 +1,11 @@
 package com.arshalif.paysera.data.db
 
 import androidx.room.Database
+import com.arshalif.paysera.data.db.entity.BalanceEntity
 
 
 @Database(
-    entities = [],
+    entities = [BalanceEntity::class],
     version = 1
 )
 abstract class BalanceDataBase {
--- app/src/main/java/com/arshalif/paysera/data/db/RoomBalance.kt
+++ app/src/main/java/com/arshalif/paysera/data/db/RoomBalance.kt
@@ -2,7 +2,7 @@ package com.arshalif.paysera.data.db
 
 import com.arshalif.paysera.data.db.entity.BalanceEntity
 
-class RoomBalance :Balance {
+class RoomBalance : Balance {
     override suspend fun getBalances(): List<BalanceEntity> {
         TODO("Not yet implemented")
     }
