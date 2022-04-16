a1b62361c770251fdfdcbb656e9af9d5eae2136b
arash <aghelifar.arash@gmail.com>
2022-04-16 20:55:33 +0430

    inject ratioApi to repositories

--- app/src/main/java/com/arshalif/paysera/data/repositories/RatioRepositoryImpl.kt
+++ app/src/main/java/com/arshalif/paysera/data/repositories/RatioRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.arshalif.paysera.data.repositories
 
+import com.arshalif.paysera.data.api.RatiosApi
 import com.arshalif.paysera.domain.repositories.RatioRepository
 import com.arshalif.paysera.view.model.ResultState
 import dagger.hilt.android.scopes.ActivityRetainedScoped
@@ -8,7 +9,7 @@ import java.math.BigDecimal
 import javax.inject.Inject
 
 @ActivityRetainedScoped
-class RatioRepositoryImpl @Inject constructor() : RatioRepository {
+class RatioRepositoryImpl @Inject constructor(ratiosApi: RatiosApi) : RatioRepository {
 
     override suspend fun getRatios(): Flow<ResultState<HashMap<String, BigDecimal>>> {
         TODO("Not yet implemented")
