189c3a7a63bf7d16a42a24fd644400e2c9260652
arash <aghelifar.arash@gmail.com>
2022-04-16 19:08:29 +0430

    add DI in UI

---
+++ app/src/main/java/com/arshalif/paysera/view/di/ViewModelsModule.kt
@@ -0,0 +1,17 @@
+package com.arshalif.paysera.view.di
+
+import com.arshalif.paysera.data.BalanceRepositoryImpl
+import com.arshalif.paysera.domain.repositories.BalanceRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+abstract class ViewModelsModule {
+
+    @Binds
+    abstract fun bindBalanceRepository(repository: BalanceRepositoryImpl): BalanceRepository
+
+}
--- app/src/main/java/com/arshalif/paysera/view/exchange/ExchangeViewModel.kt
+++ app/src/main/java/com/arshalif/paysera/view/exchange/ExchangeViewModel.kt
@@ -1,6 +1,11 @@
 package com.arshalif.paysera.view.exchange
 
 import androidx.lifecycle.ViewModel
+import com.arshalif.paysera.domain.repositories.BalanceRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ExchangeViewModel : ViewModel() {
+@HiltViewModel
+class ExchangeViewModel @Inject constructor(private val balanceRepository: BalanceRepository) :
+    ViewModel() {
 }
