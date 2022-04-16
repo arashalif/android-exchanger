e4d050502fe68c47ecde8f6b58a2f4ee49c09da2
arash <aghelifar.arash@gmail.com>
2022-04-16 17:46:11 +0430

    rename exchange to balance repository

--- app/src/main/java/com/arshalif/paysera/domain/repositories/BalanceRepository.kt
+++ app/src/main/java/com/arshalif/paysera/domain/repositories/BalanceRepository.kt
@@ -2,7 +2,8 @@ package com.arshalif.paysera.domain.repositories
 
 import com.arshalif.paysera.domain.model.Balance
 
-interface ExchangeRepository {
+interface BalanceRepository {
+    suspend fun fetchBalance(): Balance
     suspend fun fetchBalances(): List<Balance>
     suspend fun updateBalances(soldBalance: Balance, boughtBalance: Balance): List<Balance>
 }
